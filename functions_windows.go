80d97e648dd9d5a4ae94859357fdeac316d637e8
Kethsar <kethsar@touhou.cafe>
2018-11-15 00:45:23 -0800

    Add event listening for Windows; switch polling to fallback

--- functions_windows.go
+++ functions_windows.go
@@ -7,6 +7,10 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/AllenDang/w32"
+
+	"github.com/AllenDang/gform"
+
 	"github.com/atotto/clipboard"
 )
 
@@ -15,12 +19,20 @@ const (
 )
 
 var (
+	cbWin           *gform.Form
 	u32             = syscall.NewLazyDLL("user32.dll")
 	getClipSequence = u32.NewProc("GetClipboardSequenceNumber")
 	cbSeq           uintptr
 )
 
 func monitorClipboard() {
+	if createWindow() {
+		gform.RunMainLoop()
+	}
+
+	printToConsole("Error when creating window for monitoring clipboard")
+	printToConsole("Falling back to polling")
+
 	delay := time.NewTicker(500 * time.Millisecond)
 
 	for range delay.C {
@@ -33,6 +45,30 @@ func monitorClipboard() {
 				printToConsole(fmt.Sprintf("Error reading clipboard: %v", err))
 			} else {
 				syncClipoard(cb)
+			}
+		}
+	}
+}
+
+func createWindow() bool {
+	gform.Init()
+
+	cbWin = gform.NewForm(nil)
+	cbWin.Bind(w32.WM_CLIPBOARDUPDATE, cbUpdateHandler)
+
+	return w32.AddClipboardFormatListener(cbWin.Handle())
+}
+
+func cbUpdateHandler(arg *gform.EventArg) {
+	if data, ok := arg.Data().(*gform.RawMsg); ok {
+		if data.Msg == w32.WM_CLIPBOARDUPDATE {
+			if w32.IsClipboardFormatAvailable(w32.CF_UNICODETEXT) {
+				cb, err := clipboard.ReadAll()
+				if err != nil {
+					printToConsole(fmt.Sprintf("Error reading clipboard: %v", err))
+				} else {
+					syncClipoard(cb)
+				}
 			}
 		}
 	}
